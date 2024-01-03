2d88930fc9005a0dff86231ae1c711dea6093cc1
zetag33 <95145225+zetag33@users.noreply.github.com>
2024-01-03 17:23:38 +0100

    Create computer_vision.py

---
+++ computer_vision.py
@@ -0,0 +1,161 @@
+# Import PyTorch
+import torch
+from torch import nn
+
+# Import torchvision 
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Import matplotlib for visualization
+import matplotlib.pyplot as plt
+
+# Get the data in this case a simple test dataset integrated into Torchvision, FashionMIST
+
+# Setup training data
+train_data = datasets.FashionMNIST(
+    root="data", # where to download data to?
+    train=True, # get training data
+    download=True, # download data if it doesn't exist on disk
+    transform=ToTensor(), # images come as PIL format, we want to turn into Torch tensors
+    target_transform=None # you can transform labels as well
+)
+
+# Setup testing data
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False, # get test data
+    download=True,
+    transform=ToTensor()
+)
+
+# Prepare a DataLoader. Usually the batch size is a multiple of 8 due to computing efficency, I chose 32 as my batch size.
+
+from torch.utils.data import DataLoader
+
+# Setup the batch size hyperparameter
+BATCH_SIZE = 32
+
+# Turn datasets into iterables (batches)
+train_dataloader = DataLoader(train_data, # dataset to turn into iterable
+    batch_size=BATCH_SIZE, # how many samples per batch? 
+    shuffle=True # shuffle data every epoch?
+)
+
+test_dataloader = DataLoader(test_data,
+    batch_size=BATCH_SIZE,
+    shuffle=False # don't necessarily have to shuffle the testing data
+)
+
+# Creating a simple linear neural network to see the first results and if we need non-linearity
+
+from torch import nn
+class FashionMNISTModelV0(nn.Module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(), # neural networks like their inputs in vector form
+            nn.Linear(in_features=input_shape, out_features=hidden_units), # in_features = number of features in a data sample (784 pixels)
+            nn.Linear(in_features=hidden_units, out_features=output_shape)
+        )
+    
+    def forward(self, x):
+        return self.layer_stack(x)
+# We flatten the input to the model so its a vector instead of a matrix in this case instead of a [1,28,28] tensor, we input into the model a [1,756]
+
+# Instantiate the model
+
+torch.manual_seed(42)
+
+# Need to setup model with input parameters
+model_0 = FashionMNISTModelV0(input_shape=784, # one for every pixel (28x28)
+    hidden_units=10, # how many units in the hiden layer
+    output_shape=len(class_names) # one for every class
+)
+model_0.to("cpu") # keep model on CPU to begin with
+
+# Set up loss function and optimizer.
+loss_fn = nn.CrossEntropyLoss() # this is also called "criterion"/"cost function" in some places, typical multi classification problems
+optimizer = torch.optim.SGD(params=model_0.parameters(), lr=0.1)
+
+# We also define a function to measure the accuracy of our model
+def accuracy_fn(y_true, y_pred):
+    """Calculates accuracy between truth labels and predictions.
+
+    Args:
+        y_true (torch.Tensor): Truth labels for predictions.
+        y_pred (torch.Tensor): Predictions to be compared to predictions.
+
+    Returns:
+        [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
+    """
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc
+
+# Train and test the model, afterwards we encapsule this processes into functions
+
+# Import tqdm for progress bar
+from tqdm.auto import tqdm
+
+# Set the seed and start the timer
+torch.manual_seed(42)
+
+# Set the number of epochs (we'll keep this small for faster training times)
+epochs = 3
+
+# Create training and testing loop
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}\n-------")
+    ### Training
+    train_loss = 0
+    # Add a loop to loop through training batches
+    for batch, (X, y) in enumerate(train_dataloader):
+        model_0.train() 
+        # 1. Forward pass
+        y_pred = model_0(X)
+
+        # 2. Calculate loss (per batch)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss # accumulatively add up the loss per epoch 
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward
+        loss.backward()
+
+        # 5. Optimizer step
+        optimizer.step()
+
+        # Print out how many samples have been seen
+        if batch % 400 == 0:
+            print(f"Looked at {batch * len(X)}/{len(train_dataloader.dataset)} samples")
+
+    # Divide total train loss by length of train dataloader (average loss per batch per epoch)
+    train_loss /= len(train_dataloader)
+    
+    ### Testing
+    # Setup variables for accumulatively adding up loss and accuracy 
+    test_loss, test_acc = 0, 0 
+    model_0.eval()
+    with torch.inference_mode():
+        for X, y in test_dataloader:
+            # 1. Forward pass
+            test_pred = model_0(X)
+           
+            # 2. Calculate loss (accumatively)
+            test_loss += loss_fn(test_pred, y) # accumulatively add up the loss per epoch
+
+            # 3. Calculate accuracy (preds need to be same as y_true)
+            test_acc += accuracy_fn(y_true=y, y_pred=test_pred.argmax(dim=1))
+        
+        # Calculations on test metrics need to happen inside torch.inference_mode()
+        # Divide total test loss by length of test dataloader (per batch)
+        test_loss /= len(test_dataloader)
+
+        # Divide total accuracy by length of test dataloader (per batch)
+        test_acc /= len(test_dataloader)
+
+    ## Print out what's happening
+    print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
