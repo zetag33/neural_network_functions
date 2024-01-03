3ba93dc484af1d42ff00e7b7e51d93a57b8100d9
zetag33 <95145225+zetag33@users.noreply.github.com>
2024-01-03 20:00:31 +0100

    Update computer_vision.py

--- computer_vision.py
+++ computer_vision.py
@@ -160,7 +160,7 @@ for epoch in tqdm(range(epochs)):
     ## Print out what's happening
     print(f"\nTrain loss: {train_loss:.5f} | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%\n")
 
-# The train and test loss should be something around 0.45 - 0.50, which is not especially good but should see that in each training epoch the model is atleast getting better.
+# The train and test loss should be something around 0.45 - 0.50 and the accuracy should be around 80-85% which is not bad.
 
 # Setup device agnostic code
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -195,6 +195,243 @@ model_1 = FashionMNISTModelV1(input_shape=784, # number of input features
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(params=model_1.parameters(), 
                             lr=0.1)
+# I have functionalized the train and test phases in the functions.py file, I import from there the functions.
+
+from functions import train_step, test_step
+
+# Let's loop for each epoch and see the results
+
+torch.manual_seed(42)
+
+# Measure time
+from timeit import default_timer as timer
+train_time_start_on_gpu = timer()
+
+epochs = 3
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}\n---------")
+    train_step(data_loader=train_dataloader, 
+        model=model_1, 
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        accuracy_fn=accuracy_fn
+    )
+    test_step(data_loader=test_dataloader,
+        model=model_1,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_fn
+    )
+
+# The test accuracy should go around 75%, so we can see that in this case introducing non linearity does not help with the matter, probably because the clothes imagines the model tries to predict can already be defined by linear lines.
+
+# Import the eval_model function to check the model performance and compare
+
+from functions import eval_model
+
+model_0_results = eval_model(model=model_0, data_loader=test_dataloader,
+    loss_fn=loss_fn, accuracy_fn=accuracy_fn,
+    device=device
+)
+model_1_results = eval_model(model=model_1, data_loader=test_dataloader,
+    loss_fn=loss_fn, accuracy_fn=accuracy_fn,
+    device=device
+)
+print(model_0_results)
+print(model_1_results)
+
+# It seems like we are overfitting in the training data, lets try to introduce convulational layers.
+
+# Convulational neural networks are known for its efficacy in finding patterns in unstructured data such as images.
+
+# Create a convolutional neural network
+# Using the TinyVGG architecture, a typical one.
+# We use the Conv2d layers, because our data is bidimensional, height * width, if it was 1d or 3d, conv1d() and conv3d() are available.
+# The Conv2d layer can take this parameters:
+#     - in_channels (int) - Number of channels in the input image.
+#     - out_channels (int) - Number of channels produced by the convolution.
+#     - kernel_size (int or tuple) - Size of the convolving kernel/filter.
+#     - stride (int or tuple, optional) - How big of a step the convolving kernel takes at a time. Default: 1.
+#     - padding (int, tuple, str) - Padding added to all four sides of input. Default: 0.
+    
+class FashionMNISTModelV2(nn.Module):
+    """
+    Model architecture copying TinyVGG from: 
+    https://poloclub.github.io/cnn-explainer/
+    """
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape, 
+                      out_channels=hidden_units, 
+                      kernel_size=3, # how big is the square that's going over the image?
+                      stride=1, # default
+                      padding=1),# options = "valid" (no padding) or "same" (output has same shape as input) or int for specific number 
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units, 
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2) # default stride value is same as kernel_size
+        )
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(hidden_units, hidden_units, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            # Where did this in_features shape come from? 
+            # It's because each layer of our network compresses and changes the shape of our inputs data.
+            nn.Linear(in_features=hidden_units*7*7, 
+                      out_features=output_shape)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        x = self.block_1(x)
+        # print(x.shape)
+        x = self.block_2(x)
+        # print(x.shape)
+        x = self.classifier(x)
+        # print(x.shape)
+        return x
+
+# Instantiate the model.
+torch.manual_seed(42)
+model_2 = FashionMNISTModelV2(input_shape=1, 
+    hidden_units=10, 
+    output_shape=len(class_names)).to(device)
+
+# Setup loss and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model_2.parameters(), 
+                             lr=0.1)
+
+torch.manual_seed(42)
+
+# Train it and test it
+
+epochs = 3
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}\n---------")
+    train_step(data_loader=train_dataloader, 
+        model=model_2, 
+        loss_fn=loss_fn,
+        optimizer=optimizer,
+        accuracy_fn=accuracy_fn,
+        device=device
+    )
+    test_step(data_loader=test_dataloader,
+        model=model_2,
+        loss_fn=loss_fn,
+        accuracy_fn=accuracy_fn,
+        device=device
+    )
+
+# The model should output something between 86 and 90% accuracy in the test data, the highest achieved of the three models: We tried a linear one, one that had no linearity and the convolutional one.
+
+# There is a trade off between performance and computing efficiency, the convolutational model has the highest accuracy but also takes the most time.
+
+# Define a function to make predictions 
+
+def make_predictions(model: torch.nn.Module, data: list, device: torch.device = device):
+    pred_probs = []
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+            # Prepare sample
+            sample = torch.unsqueeze(sample, dim=0).to(device) # Add an extra dimension and send sample to device
+
+            # Forward pass (model outputs raw logit)
+            pred_logit = model(sample)
+
+            # Get prediction probability (logit -> prediction probability)
+            pred_prob = torch.softmax(pred_logit.squeeze(), dim=0) # note: perform softmax on the "logits" dimension, not "batch" dimension (in this case we have a batch size of 1, so can perform on dim=0)
+
+            # Get pred_prob off GPU for further calculations
+            pred_probs.append(pred_prob.cpu())
+            
+    # Stack the pred_probs to turn list into a tensor
+    return torch.stack(pred_probs)
+
+# Get 10 samples of the test data and its labels
+
+import random
+random.seed(42)
+test_samples = []
+test_labels = []
+for sample, label in random.sample(list(test_data), k=9):
+    test_samples.append(sample)
+    test_labels.append(label)
+
+# Make predictions and compare
+
+pred_probs= make_predictions(model=model_2, 
+                             data=test_samples)
+
+# Convert probabilities into labels
+
+pred_classes = pred_probs.argmax(dim=1)
+# Compare them, should match every label
+print(test_labels, pred_classes)
+
+# Let's predict again and do a confusion matrix of the model
+# Import tqdm for progress bar
+from tqdm.auto import tqdm
+
+# 1. Make predictions with trained model
+class_names = train_data.classes
+y_preds = []
+model_2.eval()
+with torch.inference_mode():
+  for X, y in tqdm(test_dataloader, desc="Making predictions"):
+    # Send data and targets to target device
+    X, y = X.to(device), y.to(device)
+    # Do the forward pass
+    y_logit = model_2(X)
+    # Turn predictions from logits -> prediction probabilities -> predictions labels
+    y_pred = torch.softmax(y_logit, dim=1).argmax(dim=1) # note: perform softmax on the "logits" dimension, not "batch" dimension (in this case we have a batch size of 32, so can perform on dim=1)
+    # Put predictions on CPU for evaluation
+    y_preds.append(y_pred.cpu())
+# Concatenate list of predictions into a tensor
+y_pred_tensor = torch.cat(y_preds)
 
 
 
+from torchmetrics import ConfusionMatrix
+from mlxtend.plotting import plot_confusion_matrix
+
+# Setup confusion matrix instance and compare predictions to targets
+confmat = ConfusionMatrix(num_classes=len(class_names), task='multiclass')
+confmat_tensor = confmat(preds=y_pred_tensor,
+                         target=test_data.targets)
+
+# 3. Plot the confusion matrix
+fig, ax = plot_confusion_matrix(
+    conf_mat=confmat_tensor.numpy(), # matplotlib likes working with NumPy 
+    class_names=class_names, # turn the row and column labels into class names
+    figsize=(10, 7)
+);
+
+# We should see the diagonal of the matrix in full blue as the majority of predictions are there, thats good and ideal model should have all the diagonal filled and the other spots as 0.
+
+# Lastly, we save the state dict of the model into an object so in case we needed again we will be able to load this state dict that contains the internal weights of the model into a model of the same characteristics.
+from pathlib import Path
+
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, # create parent directories if needed
+                 exist_ok=True # if models directory already exists, don't error
+)
+
+# Create model save path
+MODEL_NAME = "03_pytorch_computer_vision_model_2.pth"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+# Save the model state dict
+print(f"Saving model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model_2.state_dict(), # only saving the state_dict() only saves the learned parameters
+           f=MODEL_SAVE_PATH)
